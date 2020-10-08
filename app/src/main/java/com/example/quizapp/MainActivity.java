4bb5be3d8fa8d6129ae7e5cb4cfa19f99e5bcd51
hieuphan94 <phantronghieu1@gmail.com>
2020-10-08 16:28:07 +0700

    Create TextView and Pointer it

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv1 = (TextView) findViewById(R.id.tv1);
 
     }
 }
